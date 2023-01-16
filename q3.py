7b7d951d0ac85cdd391e2f8505dd3438589e804a
Abhinav Gupta <hades7963@protonmail.com>
2023-01-16 22:57:26 +0530

    Fixed q3.py

--- q3.py
+++ q3.py
@@ -1,13 +1,19 @@
-user_input = input("Enter a string: ").split(" ")
+user_input = input("Enter a string: ")
+rotate = int(input("Enter number to rotate by: "))
 string = ""
-for word in user_input:
-    for ch in word:
-        unicode = ord(ch)
-        unicode += 13
-        if unicode > 122:
-            unicode = 96 + unicode - 122
-        elif 90 < unicode < 97:
-            unicode = 64 + unicode - 90
-        string += chr(unicode)
-    string += " "
-print(string)
+for ch in user_input:
+    unicode = ord(ch)
+    if 97 <= unicode <= 122:
+        unicode += rotate
+        if unicode < 97:
+            unicode = 123 - (97 - unicode)
+        elif unicode > 122:
+            unicode = 96 + (unicode - 122)
+    elif 65 <= unicode <= 90:
+        unicode += rotate
+        if unicode < 65:
+            unicode = 91 - (65 - unicode)
+        elif unicode > 90:
+            unicode = 64 + (unicode - 90)
+    string += chr(unicode) + " "
+print(string.rstrip())
